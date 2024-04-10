accbb24f417ee34ddf4b9639392a5ea0d534d390
Seun Dada <seun.dada777@gmail.com>
2024-04-10 19:46:55 -0400

    build the conroller

---
+++ controllers/devskills.js
@@ -0,0 +1,12 @@
+// import the devskill data
+import { devskills } from '../data/devskill-data.js'
+
+function index(req, res) {
+  res.render('devskills/index', {
+    devskills: devskills
+  })
+}
+
+export{
+  index,
+}
--- routes/devskills.js
+++ routes/devskills.js
@@ -1,14 +1,9 @@
 import { Router } from 'express'
-// import the Todo data
-import { devskills } from '../data/devskills-data.js'
+import * as devskillsCtrl from '../controllers/devskills.js'
 
 const router = Router()
 
-// GET localhost:3000/todos
-app.get('/', function(req, res) {
-  res.render('devskills/index', {
-    devskillss: devskills
-  })
-})
+// GET localhost:3000/devskillss
+router.get('/', devskillsCtrl.index)
 
 export { router }
