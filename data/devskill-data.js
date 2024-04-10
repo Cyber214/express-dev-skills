8f723ea2abf8802041d5c08eb00449c49d1fb5fa
Seun Dada <seun.dada777@gmail.com>
2024-04-10 18:49:08 -0400

    update array of data

---
+++ data/devskill-data.js
@@ -0,0 +1,9 @@
+const devskills = [
+  {skill: 'Front-end', completed: true, _id: 125223},
+  {skill: 'Typescript', completed: false, _id: 127904},
+  {skill: 'Back-end', completed: false, _id: 139608},
+]
+
+export { 
+	devskills
+}
